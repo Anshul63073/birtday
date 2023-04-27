7167022cf4226f2cafd112df487334ab8cb92f20
Anshul63073 <anshulgajariya@gmail.com>
2023-04-27 21:59:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import Invitation from './components/Invitation';
+import React, { useState } from "react";
 
-function App() {
+function LuckyNumberGame() {
+  const [luckyNumber, setLuckyNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [guess, setGuess] = useState("");
+  
+
+  const handleGuess = (event) => {
+    event.preventDefault();
+    const numGuess = parseInt(guess);
+    if (isNaN(numGuess)) {
+      alert("Please enter a valid number.");
+      return;
+    }
+    if (numGuess < luckyNumber) {
+      alert('You guessed smaller number. Try a bigger number.');
+    } else if (numGuess > luckyNumber) {
+      alert('You guessed bigger number. Try a smaller number.');
+    } else {
+      alert('Congratulations! You guessed the right number');
+    }
+    setGuess("");
+  };
+
   return (
-    <div className="App">
-      <Invitation
-  subject="Birthday Party Invitation"
-  to="jaGdish@gmail.com"
-  name="jaGdish"
-  date="next Friday"
-  location="Green field Avenue"
-  friends="Ritu, Saurabh, Kartik"
-/>
-
+    <div>
+      <h1>Lucky Number Game</h1>
+      <p>Guess a number between 1 and 100.</p>
+      <form onSubmit={handleGuess}>
+        <input type="text" value={guess} onChange={(event) => setGuess(event.target.value)} />
+        <button type="submit">Guess</button>
+      </form>
+      
     </div>
   );
 }
 
-export default App;
+export default LuckyNumberGame;
